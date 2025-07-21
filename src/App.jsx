fb4eb683b40f5873c7c537b3efaf457967e4d475
robertsassarovskis <assarovskisr@gmail.com>
2025-07-21 04:24:17 +0300

    Update website content

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,11 @@
 import { BrowserRouter as Router } from 'react-router-dom'
-import Header from './components/Header'
 import NavBar from './components/NavBar'
 import AppRoutes from './router/Routes'
 import Footer from './components/Footer'
-import './i18n' // ← this line is important
 
 export default function App() {
   return (
-    <Router>
+    <Router basename="/meness-boulings">
       <div className="min-h-screen flex flex-col bg-purple-50">
         <NavBar />
         <main className="flex-grow">
